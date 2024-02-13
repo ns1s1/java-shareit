4eb2eab1efb3abbffa7f804e3640583d5f03bf6a
SSS <tinkywf@yandex.ru>
2024-02-13 15:18:54 +0300

    fix

--- src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -4,7 +4,6 @@ import ru.practicum.shareit.user.dto.UserCreateDto;
 import ru.practicum.shareit.user.dto.UserResponseDto;
 import ru.practicum.shareit.user.model.User;
 
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
